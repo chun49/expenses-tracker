0fdfbbeeeb999d98aefa66b4e62f5f628cac138f
heliusindrawan49 <heliusindrawan49@gmail.com>
2020-10-09 10:49:14 +0700

    master

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,11 +1,64 @@
 <script>
-	export let name;
+  import axios from "axios";
+  import { onMount } from "svelte";
+  let input = 0;
+  let typeOfTransaction = "+";
+  let transactions = [];
+  onMount(async () => {
+    const { data } = await axios.get("/api/transactions/");
+    transactions = data;
+  });
+  const addTransaction = async () => {
+    const transaction = {
+      date: new Date().getTime(),
+      value: typeOfTransaction === "+" ? input : input * -1,
+    };
+    const response = await axios.post("/api/transactions", transaction);
+    transactions = [response.data, ...transactions];
+    input = 0;
+  };
+  const removeTransaction = async (id) => {
+    const response = await axios.delete("/api/transactions/" + id);
+    if (response.data.id === id) {
+      transactions = transactions.filter((t) => t.id !== id);
+    }
+  };
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  .app {
+    margin: 40px auto;
+    max-width: 500px;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<div class="app container">
+  <div class="field has-addons">
+    <p class="control">
+      <span class="select">
+        <select bind:value={typeOfTransaction}>
+          <option value="+">+</option>
+          <option value="-">-</option>
+        </select>
+      </span>
+    </p>
+    <p class="control is-expanded">
+      <input
+        class="input"
+        type="number"
+        bind:value={input}
+        placeholder="Amount of money" />
+    </p>
+    <p class="control">
+      <button class="button" on:click={addTransaction}>Save</button>
+    </p>
+  </div>
+  {#each transactions as transaction (transaction._id)}
+    <div class="notification">
+      {transaction.value}
+      <button
+        class="delete"
+        on:click={() => removeTransaction(transaction._id)}>x</button>
+    </div>
+  {/each}
+</div>
--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-const express = require('express');
-const app = express()
-const port = 3000
-app.get('/',(req,res) => res.send('Hello World'))
-app.listen(port ,() => console.log('Express is running at port ' + port))
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const app = express();
+dotenv.config({ path: ".env" });
+const { port, mongoURI } = require("./config.js");
+const transactionsRoutes = require("./routes/transactions.js");
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is Connected"))
+  .catch((err) => console.log(err));
+app.use("/api/transactions", transactionsRoutes);
+app.get("/", (req, res) => res.send("Hello World"));
+app.listen(port, () => console.log("Express is running at port " + port));
