da95f9d5e5ed47fb14efc3a514919b6604d6e0ea
heliusindrawan49 <heliusindrawan49@gmail.com>
2020-10-09 15:57:30 +0700

    first commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require('path');
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
@@ -20,5 +21,11 @@ mongoose
   .then(() => console.log("MongoDB is Connected"))
   .catch((err) => console.log(err));
 app.use("/api/transactions", transactionsRoutes);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/public'))
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname,'client','index.html'))
+  })
+}
 app.get("/", (req, res) => res.send("Hello World"));
 app.listen(port, () => console.log("Express is running at port " + port));
